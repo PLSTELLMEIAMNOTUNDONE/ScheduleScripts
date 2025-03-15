a8fc855fc322031252fb235b45bf55e520a8fe8a
PLSTELLMEIAMNOTUNDONE <st097179@student.spbu.ru>
2025-03-15 22:46:09 +0300

    testing

---
+++ src/python/testing/testing.py
@@ -0,0 +1,35 @@
+import random
+from random import choice
+
+from common.json.schedule_parser import map_spbu_data_to_schedule
+from pipeline.pipeline import process
+from schedule.config import default_weeks
+from schedule.schedule_state import SchState
+
+
+#subject			type	teacher				hours(int)			group
+class TestingSampleGenerate:
+    def __init__(self, size: int):
+        self._size = size
+        self._id = random.randint(1, self._size)
+        self._next_sample_id = 0
+        self._possible_hours = [default_weeks * 2, default_weeks * 3, default_weeks * 4]
+
+    def _generate_test_row(self) -> str:
+        hours = choice(self._possible_hours)
+        sample_id = self._next_sample_id
+        self._next_sample_id += 1
+        return f'subject_{sample_id} type_{sample_id} teacher_{sample_id} {hours} group_{sample_id}'
+
+    def generate_test_sample(self) -> SchState:
+        with open(f'C:\\Users\\lera\\PycharmProjects\\ScheduleScripts\\src\\python\\resources\\test_{self._id}.txt', 'w+') as f:
+            for _ in range(self._size):
+                f.write(self._generate_test_row() + "\n")
+        return map_spbu_data_to_schedule(f'src\\python\\resources\\test_{self._id}')
+
+
+if __name__ == '__main__':
+    sample = TestingSampleGenerate(15).generate_test_sample()
+    process([sample])
+
+
